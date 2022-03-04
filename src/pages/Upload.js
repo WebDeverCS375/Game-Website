9a2d5e2bb16ac5f2cafaa2dbef9642f1e1f07cde
ra697 <ra697@cs.drexel.edu>
2022-03-04 15:21:08 -0500

    Upload.js 3/4/2022

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,14 +1,43 @@
-import InnerHTML from 'dangerously-set-html-content'
-import React from 'react';
-import __html from "./upload.html";
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const hostname = "localhost";
+
+app.use(express.static("public"));
+
+const image_input = document.querySelector("#image_input"); //get image input
+var uploaded_image = "";
+
+image_input.addEventListener("change", function()  
+{
+   const reader = new FileReader(); //use file reader object to read file
+   reader.addEventListener("load", () => //add event listener load
+   {
+     uploaded_image = reader.result; //set global variable to uploaded file in reader
+     document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`; //set display_image element to uploaded image
+    });
+   reader.readAsDataURL(this.files[0]);
+});
 
 
-
-class Upload extends React.Component {
-    render() {
-        return <InnerHTML html={__html} />;
+app.post("/upload",function(req,res)
+{
+   let body = req.body;
+    if (
+        !body.hasOwnProperty("title") ||
+        !body.hasOwnProperty("description") ||
+        !body.hasOwnProperty("price") ||
+        !body.hasOwnProperty("category") ||
+        body.name === "" ||
+      body.description === "" 
+        
+    ) {
+        return res.sendStatus(400);
     }
-};
+    return res.redirect('/home'); //not sure where the data is supposed to be sent so I just used home as a placeholder
+});
 
-export default Upload;
-
+app.listen(port, hostname, () => {
+    console.log(`Listening at: http://${hostname}:${port}`);
+});
