f5a29ffc65fad875640eb66c56b40576d7d04d2e
ra697 <ra697@cs.drexel.edu>
2022-03-04 15:16:46 -0500

    info.js 3/4/2022

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,14 +1,30 @@
-import React from "react";
-import __html from "./Info.html";
-var template = { __html: __html };
 
-class Info extends React.Component {
-    render() {
-        return <div dangerouslySetInnerHTML={template} />;
-    };
+const express = require("express");
+const app = express();
 
-}
+const port = 3000;
+const hostname = "localhost";
+
+app.use(express.static("public"));
 
 
-export default Info;
+app.post("/Info",function(req,res)
+{
+   let body = req.body;
+    if (
+        
+        !body.hasOwnProperty("review") ||
+      body.review=== "" 
+        
+    ) {
+        return res.sendStatus(400);
+    }
+    return res.redirect('/home'); //not sure where the data is supposed to be sent so I just used home as a placeholder
+});
 
+
+
+
+app.listen(port, hostname, () => {
+    console.log(`Listening at: http://${hostname}:${port}`);
+});
