a7b1fe6001c6a9b2b380bcc3340cd8f98fc70a65
ra697 <ra697@cs.drexel.edu>
2022-03-08 16:07:13 -0500

    small code swap

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -4,39 +4,27 @@ const app = express();
 const port = 3000;
 const hostname = "localhost";
 
-app.use(express.static("public"));
 
-const image_input = document.querySelector("#image_input"); //get image input
-var uploaded_image = "";
 
-image_input.addEventListener("change", function()  
+app.use(express.json());
+
+
+
+
+app.get("/upload",function(req,res)
 {
-   const reader = new FileReader(); //use file reader object to read file
-   reader.addEventListener("load", () => //add event listener load
-   {
-     uploaded_image = reader.result; //set global variable to uploaded file in reader
-     document.querySelector("#display_image").style.backgroundImage = `url(${uploaded_image})`; //set display_image element to uploaded image
-    });
-   reader.readAsDataURL(this.files[0]);
+	let title = req.body.title;
+	let description = req.body.description;
+	let price = req.body.price;
+	let category = req.body.category;
+	
+
+	
+	res.json();
 });
 
 
-app.post("/upload",function(req,res)
-{
-   let body = req.body;
-    if (
-        !body.hasOwnProperty("title") ||
-        !body.hasOwnProperty("description") ||
-        !body.hasOwnProperty("price") ||
-        !body.hasOwnProperty("category") ||
-        body.name === "" ||
-      body.description === "" 
-        
-    ) {
-        return res.sendStatus(400);
-    }
-    return res.redirect('/home'); //not sure where the data is supposed to be sent so I just used home as a placeholder
-});
+
 
 app.listen(port, hostname, () => {
     console.log(`Listening at: http://${hostname}:${port}`);
